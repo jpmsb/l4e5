54e0bc3df4f000857420ea358879da8477b6a771
jpmsb <jpcatarinense@gmail.com>
2018-10-15 13:49:14 -0300

    l4e5

--- main.cpp
+++ main.cpp
@@ -1,46 +1,83 @@
-/* 
- * File:   main.cpp
- * Author: sobral
- *
- * Created on 8 de Outubro de 2016, 19:05
- */
-
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <prglib.h>
- 
-using namespace std;
- 
-using prglib::arvore;
-using prglib::lista;
- 
-/*
- * 
- */
-int main(int argc, char** argv) {
- 
-    //arvore<int> * a = new arvore<int>(10);
-    auto a = new arvore<int>(10);
- 
-    a->adiciona(5);
-    a->adiciona(7);
-    a->adiciona(2);
-    a->adiciona(13);
-    a->adiciona(11);
-    a->adiciona(15);
- 
-    lista<int> l;
-    a->listeInOrder(l);
- 
-    cout << "In-order: ";
-    l.escrevaSe(cout, ", ");
-    cout << endl;
+#include "prglib.h" 
 
-    while (a->tamanho() > 0) {
-      int x = a->obtemMaior();
-      cout << x << endl;
-      a->remove(x);
+using namespace std;
+using prglib::lista;
+using prglib::arvore;
+
+struct Dados {
+    string palavra;
+    int valor;
+    
+    Dados(){
+        valor = 0;
     }
- 
-    return 0;
+    
+    Dados(const string & umaPalavra, int umValor){
+        palavra = umaPalavra;
+        valor = umValor;
+    }
+    
+    bool operator<(const Dados & o){
+        return valor < o.valor;
+        //if (palavra == o.palavra){
+        //    return valor < o.valor;
+        //}
+        //return palavra < o.palavra;
+    }
+    
+    bool operator==(const Dados & o){
+        //return valor == o.valor;
+        if (palavra == o.palavra){
+            return valor == o.valor;
+        }
+        return valor == o.valor;
+        //return palavra == o.palavra;
+    }
+};
+
+ostream & operator<<(ostream & out, const Dados & x){
+    out << x.palavra << ", " << x.valor;
+    return out;
+}
+
+int main(int argc, char * argv[]) {
+    string nome_do_arquivo, palavra;
+    int n = 0;
+    nome_do_arquivo = argv[1];
+    //getline(cin, nome_do_arquivo);
+    
+    ifstream arquivo(nome_do_arquivo);
+    
+    arquivo >> palavra;
+    
+    arvore<Dados> * arv = new arvore<Dados>(Dados(palavra, n));
+    lista<Dados> lista_da_arv;
+    //lista<string> lista_da_arv;
+    
+    while (arquivo >> palavra){
+        n++;
+        try {
+            arv->obtem(palavra);
+        } catch (...) {
+            
+        }
+
+        arv->adiciona(Dados(palavra, n));
+        cout << n << endl;
+        //lista_da_arv.anexa(palavra);
+    }
+    arv->iniciaPeloFim();
+    
+    int c = 0;
+    while(! arv->inicio()){
+        //if (c == 5) break;
+        lista_da_arv.insere(arv->anterior());
+        c++;
+    }
+    
+    //lista_da_arv.ordena();
+    lista_da_arv.escrevaSe(cout);
 }
